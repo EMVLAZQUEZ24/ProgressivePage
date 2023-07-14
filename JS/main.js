8f7f601165237528f49e2dfc11defcf3a8a741e8
Ezequiel Martin Velázquez <ezequiel.velazquez.contacto@gmail.com>
2023-07-14 15:01:53 -0300

    Modificación

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,29 @@
-document.getElementById("header").innerHTML=`<form class="d-flex my-2 my-lg-0">
-<input class="form-control me-sm-2" type="text" placeholder="Search">
-<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-</form>`
+document.getElementById("header").innerHTML=`<nav id="navabar" class="navbar navbar-expand-lg bg-body-tertiary">
+<div class="container-fluid">
+  <a class="navbar-brand" href="#">ProgressivePage</a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
+  aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#home">Home</a>
+        </li>
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#about">¿Qué es progressivePage?</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#albums">Álbumes progresivos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#hystory">Historia del rock progresivo</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#contact">Contactanos</a>
+      </li>
+    </ul>
+  </div>
+</div>
+
+</nav>`
